d8e33dedfad4ab1697703e780e363ac06b023b82
smpabel <smp.pabel@gmail.com>
2021-12-31 01:17:55 +0600

    list and dict update

--- dict1.py
+++ dict1.py
@@ -1,21 +1,21 @@
 # nested dict
-dict1 = {
-    1:{101:"a",102:"b",103:"c"},
-    2:{104:"d",105:"e",101:"f"}
-}
+# dict1 = {
+#     1:{101:"a",102:"b",103:"c"},
+#     2:{104:"d",105:"e",101:"f"}
+# }
 
-for i in dict1:
-    print(i)
+# for i in dict1:
+#     print(i)
 
-for i in dict1:
-    for j in dict1[i]:
-        print(j,"=",dict1[i][j])
-    print()
+# for i in dict1:
+#     for j in dict1[i]:
+#         print(j,"=",dict1[i][j])
+#     print()
 
-a = {101:"pabel",102:"helal",103:"karim"}
-print(a)
-print(type(a))
-print()
+# a = {101:"pabel",102:"helal",103:"karim"}
+# print(a)
+# print(type(a))
+# print()
 
 # n = int(input("How many dict u want to input: "))
 # for i in range(n):
@@ -23,22 +23,32 @@ print()
 #     v = input("Enter value: ")
 #     a.update({k:v})
 
-print() 
-print(a)
-print() 
+# print() 
+# print(a)
+# print() 
 
-run = True
-def search():
+# run = True
+# def search():
 
-    m = int(input("Enter Id for search value: "))
-    for key,value in a.items():
-        global run
-        if m == key:
-            print(m,"value is: ",value)
-        else:
-            print("wrong ID") 
-    run = False  
+#     m = int(input("Enter Id for search value: "))
+#     for key,value in a.items():
+#         global run
+#         if m == key:
+#             print(m,"value is: ",value)
+#         else:
+#             print("wrong ID") 
+#     run = False  
             
 
-while run:
-    search()
+# while run:
+#     search()
+
+
+a =  {101:"pabel",102:"helal",103:"karim"}
+n = int(input("How many dict u want to input: "))
+for i in range(n):
+    k = int(input("Enter key Id: "))
+    v = input("Enter value: ")
+    a.update({k:v})
+id =  int(input("Enter ID for search: "))
+print(a[id])
--- listt.py
+++ listt.py
@@ -1,18 +1,31 @@
-lst = [10,20,30,40]
-print(lst)
-n = int(input("enter how many value do u want "))
-for i in range(0,n):
-    ele = (input())
-    lst.append(ele)
-print(lst)
-# name = lst[0]
-# age = lst[1]
-# gpa = lst[2]
-# print("name=",name,"age ", age, "GPA= ",gpa)
-# lst.insert(1,15)
+# lst = [10,20,30,40]
 # print(lst)
-lst.pop(-2)
-print(lst)
-lst.sort()
-print(lst)
-# print("minimum number is: ", lst[0])
+# n = int(input("enter how many value do u want "))
+# for i in range(0,n):
+#     ele = (input())
+#     lst.append(ele)
+# print(lst)
+# # name = lst[0]
+# # age = lst[1]
+# # gpa = lst[2]
+# # print("name=",name,"age ", age, "GPA= ",gpa)
+# # lst.insert(1,15)
+# # print(lst)
+# lst.pop(-2)
+# print(lst)
+# lst.sort()
+# print(lst)
+# # print("minimum number is: ", lst[0])
+
+list1 = []
+a = int(input("How many value do u want to input: "))
+for i in range(0,a):
+    ele = int(input("ebter value: "))
+    list1.append(ele)
+print(list1)
+if 10 in list1:
+    print("Yes 10 is in the list")
+else:
+    print("No 10 is not in the list")
+    
+ 
