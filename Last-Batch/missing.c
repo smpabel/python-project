eadb72b0eb4cfb2a39c7700ca20c5d62612c631c
smpabel <smp.pabel@gmail.com>
2022-06-20 17:51:45 +0600

    missing, sum, avg

--- Last-Batch/missing.c
+++ Last-Batch/missing.c
@@ -1,6 +1,42 @@
 #include<stdio.h>
 int main(){
-    printf("hello world");
     
+    int i,n,sum=0, missing;
+    printf("\nEnter the size of array ");
+    scanf("%d",&n);
+    int arr[n-1];
+    printf("Enter value to add in the array \n");
+    for ( i = 0; i < n-1; i++)
+    {
+        /* code */
+        scanf("%d",&arr[i]);
+        sum= sum+arr[i];
+    }
+    printf("summation of entered array is = %d",sum);
+    //find missing number tricks
+    missing = (n*(n+1))/2;
+    printf("\nsummation of actual n number is %d and  missing number is = %d",missing, missing-sum);
+
+    
+
+    //given array summation and avarage tricks
+
+    float s= 0;
+    float values[10]={1,2,3,4,5,6,7,8,9,10};
+  
+    for(int i = 0; i <10; ++i) {
+
+        s = s+values[i];
+    }
+    printf("\n\n\nsummation of given array is : %.2f", s);
+    float avg = s/10;
+    printf("\nAverage of given array is : %.2f", avg);
+
+
+
+
+return 0;
+    
+
 
 }
