08ede43661617ddaf24bed01b069b6f85d4425ae
smpabel <smp.pabel@gmail.com>
2022-07-22 12:30:33 +0600

    fdghdfghdfhdfhdfh

---
+++ Last-Batch/arry_traverse.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+main(){
+    int arr[6] = {1,2,5,6,8,9};
+    int i, n=6;
+    for (i = 0; i <n; i++)
+    {
+        printf("array position is arr[%d] and value is %d\n", i,arr[i]);
+    }
+    
+}
